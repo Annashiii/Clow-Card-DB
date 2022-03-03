42f119fd35757b203711d072ddfd3362581886f4
HuniDev <briannachogan@gmail.com>
2022-03-03 20:05:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h1 className="title"> Clow Card Database </h1>
+        <Card />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Card() {
+  return (
+    <div className='info-card'>
+    <img       src='https://i.pinimg.com/originals/01/e4/ee/01e4ee43080af552fe6a6edfeaa5fb85.jpg' />
+      <h1> English Name </h1>
+      <p> Spanish Name</p>
+      <p>Kanji</p>
+      <p>Romaji</p>
+    </div>
+  )
+}
